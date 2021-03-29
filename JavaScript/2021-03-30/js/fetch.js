40547c52b7e38e435624b21ae2b9753c33fcedaa
eomdayeon <69031678+eomdayeon@users.noreply.github.com>
2021-03-30 01:17:08 +0900

    기존에 make한 resume에 AJax 및 Fetch API 적용
    Ajax 및 Fetch API 적용 실습(아파치 웹서버 설치 및 웹서버 공부)

---
+++ JavaScript/2021-03-30/js/fetch.js
@@ -0,0 +1,8 @@
+function fetchPage(name) {
+    fetch(name).then(function(response){
+        response.text().then(function(text){
+            document.querySelector('#article').innerHTML = text;
+        })
+
+    })
+}
